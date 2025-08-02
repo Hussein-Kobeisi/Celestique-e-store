0f4739c631729d7e76204240894efe004f970cc8
HKob25 <husseinakobeisi@gmail.com>
2025-08-02 14:52:46 +0300

    Added User Tests

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -62,7 +62,6 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -73,7 +72,6 @@ class User extends Authenticatable implements JWTSubject
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile')->nullable();
             $table->string('password');
             $table->tinyInteger('role')->default(0);
             $table->integer('total_orders')->default(0);
