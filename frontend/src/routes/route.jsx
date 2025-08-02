da4abe35e9d2928fe90f62954af5059ddfab8a4c
MounirPrograms <mounirmerhebi21@gmail.com>
2025-08-02 21:00:45 +0300

    initial profile page completed

--- frontend/src/routes/route.jsx
+++ frontend/src/routes/route.jsx
@@ -1,11 +1,12 @@
 import{Routes, Route} from "react-router-dom";
 import Auth from "../Pages/Auth";
-
+import Profile from "../Pages/Profile"; 
 
 const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/auth" element={<Auth/>} />
+      <Route path="/Profile" element={<Profile/>} />
     </Routes>
   );
 };
