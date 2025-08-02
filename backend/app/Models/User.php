0462bed1d04d58d0c146fd87b7e811d06f6e9cbb
HKob25 <husseinakobeisi@gmail.com>
2025-08-02 12:34:39 +0300

    Created Session migration + fixed User Model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,11 +6,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey(); // usually the user's id
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * The attributes that are mass assignable.
